62a18f76a7513d346188d228adb211b4c6dc39f6
Prashad Nath <93466156+Prashad01@users.noreply.github.com>
2025-01-02 10:16:32 +0530

    upload the all files

---
+++ public/script.js
@@ -0,0 +1,4 @@
+// function openExam(examName) {
+//     // Example: Redirect to a specific test page based on the exam name
+//     window.location.href = `${examName}.html`; // Assuming the test page is named as the exam name (e.g., APSC.html, APDCL.html)
+// }
